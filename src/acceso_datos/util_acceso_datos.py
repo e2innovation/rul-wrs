2cb0beb614cf820cb560b67f960c3dbb8e060c2a
Juan Marquinho Vilca Castro <juanuy1985@gmail.com>
2020-04-13 00:40:11 -0500

    Fixing the project

--- src/acceso_datos/util_acceso_datos.py
+++
@@ -1,9 +0,0 @@
-import copy
-import json
-
-class UtilDatos:
-    @staticmethod
-    def convertir_campo_json_a_objeto(objeto, campo):
-        nuevo_objeto = copy.deepcopy(objeto)
-        nuevo_objeto[campo] = json.loads(objeto[campo])
-        return nuevo_objeto
